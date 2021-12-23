5efa6709303bb8daab1ee1d30e998dba4965bb4b
SeJin-Ahn <greetings07@naver.com>
2021-12-23 16:01:16 +0900

    211223

---
+++ project1/src/main/java/egovframework/example/sample/service/DevCateVO.java
@@ -0,0 +1,25 @@
+package egovframework.example.sample.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DevCateVO {
+	
+	private int tier;
+	//카테고리 티어
+	private String d_id;
+	//사번
+	private String d_name;
+	//개발자성명
+	private String d_tier;
+	//등급
+	private String d_jobgrade;
+	//직급
+	private String d_type;
+	//직원구분여부
+	
+}
